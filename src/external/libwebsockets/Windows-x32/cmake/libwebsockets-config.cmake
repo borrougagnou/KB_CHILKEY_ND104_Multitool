9e167132afd6b66c117c1c3a95aa429ca6095627
borrougagnou <borrougagnou@github.com>
2026-08-22 00:55:58 +0200

    LibWebSockets-4.1-build into the repo

---
+++ src/external/libwebsockets/Windows-x32/cmake/libwebsockets-config.cmake
@@ -0,0 +1,23 @@
+# - Config file for lws
+
+# It defines the following variables
+#  LIBWEBSOCKETS_INCLUDE_DIRS - include directories for FooBar
+#  LIBWEBSOCKETS_LIBRARIES    - libraries to link against
+
+# Get the path of the current file.
+get_filename_component(LWS_CMAKE_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
+
+# Set the include directories.
+set(LIBWEBSOCKETS_INCLUDE_DIRS "")
+
+# Include the project Targets file, this contains definitions for IMPORTED targets.
+include(${LWS_CMAKE_DIR}/LibwebsocketsTargets.cmake)
+
+# IMPORTED targets from LibwebsocketsTargets.cmake
+set(LIBWEBSOCKETS_LIBRARIES websockets websockets_shared)
+
+# These are additional libs that lws wants your app to also link to
+foreach(item "D:/a/KB_CHILKEY_ND104_Multitool/KB_CHILKEY_ND104_Multitool/src/external/libwebsockets/Windows-x32/lib/libzlibstatic.a;ws2_32.lib;userenv.lib;psapi.lib;iphlpapi.lib;crypt32.lib;D:/a/KB_CHILKEY_ND104_Multitool/KB_CHILKEY_ND104_Multitool/src/external/libwebsockets/Windows-x32/lib/libzlibstatic.a")
+	list(APPEND LIBWEBSOCKETS_DEP_LIBS ${item})
+endforeach()
+
